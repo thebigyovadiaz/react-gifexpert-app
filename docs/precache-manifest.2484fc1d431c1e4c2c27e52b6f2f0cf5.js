66f8df6034b3528ba7ff7a395e994aecd3f92607
Yovanny Diaz <thebigyovadiaz@gmail.com>
2020-07-13 21:42:06 -0400

    Upload Docs of build app

---
+++ docs/precache-manifest.2484fc1d431c1e4c2c27e52b6f2f0cf5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "113ee4bb54bc6bb7100ea1506df0d2ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bcf3f073e8f19f918f8d",
+    "url": "/static/css/main.ac045064.chunk.css"
+  },
+  {
+    "revision": "3ec50c8fead662706b17",
+    "url": "/static/js/2.637c77a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.637c77a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcf3f073e8f19f918f8d",
+    "url": "/static/js/main.95c3cbc8.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
